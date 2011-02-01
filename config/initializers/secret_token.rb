d45626b4c734c299ddcacbe33345963596b178b0
len <len@ubuntu.(none)>
2011-02-01 03:07:47 -0800

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab2::Application.config.secret_token = '672dbfbb09dc4d61507117ebe23de6b3c5fd7c929320d98003e5dadeba2804967139c892ea808cdb6cb62bf25fde73591df157ef02a5ea5d51e0ade7cc38d93e'
